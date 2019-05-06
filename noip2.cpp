8cffaca0eded4e5a8ee4c96248b36baa1c7cd159
Felix Zhu <kempning>
2019-05-06 17:31:49 +0800

    second problem (unfinish)

---
+++ noip2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+void prime(int n);
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    prime(n);
+
+}
+void prime(int n){
+    for(int i = 2; i < n; i++)
+    for(int j = 2; j < i; j++)
+    {
+        
+    }
+    
+    
+}
