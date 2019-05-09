e6d7589ad4564ac7a0bf2ffe61edc9a02d952947
kempning <42160738+kempning@users.noreply.github.com>
2019-05-09 18:25:32 +0800

    Add files via upload

--- noip2.cpp
+++ noip2.cpp
@@ -1,23 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define N 1120
+#define K 14
 int getsum(int n,int k);
 int main(int argc, char** arg)
 {
-    int prime[1120][2],i=0,n=0,k=0;
+    int prime[1120],size=0,n=0,k=0;
     string fileName = "prime.txt";
-
+    int dp[1121][15];memset(dp,0,sizeof(dp));
+    dp[0][0]=1;
     ifstream prime_input (fileName.c_str());
     if(!prime_input){
         cout<<"can't open file:"<<fileName<<endl;
         return 0;
     }
     while(!prime_input.eof()){
-        prime_input >> prime[i++][0];
+        cout<<size<<endl; 
+        prime_input >> prime[size];
+        size++;
+        
     }
     cin>>n>>k;
-    cout<<getsum(n,k)<<endl;
-}
+    
+    for(int i=0;i<size;i++){    
+        for(int j=14;j>=1;j--){    
+           for(int m=1120;m>=prime[i];m--){
+               dp[m][j]=dp[m][j]+dp[m-prime[i]][j-1];
+            }
+        }
+    }
+    cout<<dp[n][k]<<endl;
 
-int getsum(int n,int k){
-    if()
+    //cout<<getsum(n,k)<<endl;
 }
--- prime.cpp
+++ prime.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int main(){
     freopen("prime.txt","w",stdout);
+    cout<<"2"<<endl;
     for(int i = 2; i < 1120; i++){
-        for(int j = 2; j < i; j++){
+        for(int j = 2; j <= i; j++){
             if(i % j == 0){
                 break;
             }        
